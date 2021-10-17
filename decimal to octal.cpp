61cba8aee6ee451feae42b42783dd20ae23165a5
abhyuday2003 <92596193+abhyuday2003@users.noreply.github.com>
2021-10-17 11:45:16 +0530

    Update decimal to octal.cpp

--- decimal to octal.cpp
+++ decimal to octal.cpp
@@ -2,13 +2,13 @@
 #include <math.h>
 int decimalToOctal(int decimalnum)
 {
-    int octalnum = 0, temp = 1;
+    int octalnum = 0, t = 1;
 
     while (decimalnum != 0)
     {
-    	octalnum = octalnum + (decimalnum % 8) * temp;
+    	octalnum = octalnum + (decimalnum % 8) * t;
     	decimalnum = decimalnum / 8;
-        temp = temp * 10;
+        t = t * 10;
     }
 
     return octalnum;
